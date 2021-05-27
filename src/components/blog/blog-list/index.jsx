0340f4bb9acdf9e0a80723487e8dba64cf0bd82a
ALStoilkov <antonistoilkov@gmail.com>
2021-05-27 21:43:40 +0100

    fetch blogs works

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
+
 export default class BlogList extends Component {
+  state = { posts: [] };
+  componentDidMount = async () => {
+    // console.log(process.env.REACT_APP_LOCAL_BE);
+    const res = await fetch(process.env.REACT_APP_LOCAL_BE + "/blogPosts");
+    const posts = await res.json();
+    // console.log(posts);
+    this.setState({ posts });
+  };
+
   render() {
     return (
       <Row>
-        {posts.map((post) => (
+        {this.state.posts.map((post) => (
           <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
+            <BlogItem key={post._id} {...post} />
           </Col>
         ))}
       </Row>
